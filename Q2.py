c2a72517a6fff20cb1bdbc96cfb33e5d05e8c365
agoshu <70455535+agoshu@users.noreply.github.com>
2020-09-15 21:30:48 -0500

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,12 @@
+#Abel Goshu
+#1675552
+
+user_num=int(input("Enter integer:\n"))
+squared=user_num*user_num
+cubed=squared*user_num
+print("You entered:",user_num)
+print(user_num,"squared is",squared)
+print("And",user_num,"cubed is",cubed,"!!")
+user_num2=int(input("Enter another integer:\n"))
+print(user_num,"+",user_num2,"is",user_num+user_num2)
+print(user_num,"*",user_num2,"is",user_num*user_num2)
